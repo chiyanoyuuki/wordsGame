b30cec045936f93dab0fd3981ac8ac494c24984a
chiyanoyuuki <charles.poure@hotmail.com>
2022-12-18 20:19:57 +0100

    first

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,16 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import DATA from '../assets/data.json';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
+  public data: any = DATA;
   title = 'words';
+
+  ngOnInit() {
+    console.log(this.data);
+  }
 }
